db7decb4b4f4aa4f30da88811ea237aeea03a1e2
maSchoeller <git@marvinschoeller.de>
2021-01-05 16:56:07 +0100

    [dev]: add basic dotnet structure

---
+++ src/node/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lightning.Node
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
